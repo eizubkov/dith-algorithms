b15f0da3ecff0d070bd95d7692ef9965b31b0d86
Elisey Zubkov <zubkov.ei@phystech.edu>
2019-09-18 00:38:24 +0300

    module01:C: fix code-style

--- module01/C/main.cpp
+++ module01/C/main.cpp
@@ -3,46 +3,46 @@
 #include <string>
 
 
-void Find(const std::string & haystack, const std::string & needle) {
+void PrintEntries(const std::string & haystack, const std::string & needle) {
 
-    int const haystack_size = haystack.size();
-    int const needle_size = needle.size();
+    const int haystack_size = haystack.size();
+    const int needle_size = needle.size();
 
-    std::vector<int> z (needle_size, 0);
+    std::vector<int> z(needle_size, 0);
 
     for (int i = 1; i < needle_size; ++i) {
-        int curr = 0;
-        while (i + curr < needle_size && needle[curr + i] == needle[curr]) {
-            ++curr;
+        int shift = 0;
+        while (i + shift < needle_size && needle[shift + i] == needle[shift]) {
+            ++shift;
         }
-        z[i] = curr;
+        z[i] = shift;
     }
 
     int left = 0;
     int right = 0;
 
     for (int i = 0; i < haystack_size; ++i) {
-        int curr = i <= right ? std::min(z[i - left], right - i + 1) : 0;
-        while (curr + i < haystack_size
-               && curr < needle_size
-               && (haystack[i + curr] == needle[curr]
-                   || (needle[curr] == '?'))) {
-            ++curr;
+        int shift = (i <= right) ? std::min(z[i - left], right - i + 1) : 0;
+        while (shift + i < haystack_size && shift < needle_size
+               && (haystack[i + shift] == needle[shift]
+               || (needle[shift] == '?'))) {
+            ++shift;
         }
-        if (curr == needle_size) {
+        if (shift == needle_size) {
             std::cout << i << " ";
         }
-        if (curr + i - 1 > right) {
+        if (shift + i - 1 > right) {
             left = i;
-            right = curr + i - 1;
+            right = shift + i - 1;
         }
     }
+    std::cout << std::endl;
 }
 int main() {
     std::string needle, haystack;
     std::cin >> needle >> haystack;
 
-    Find(haystack, needle);
+    PrintEntries(haystack, needle);
 
     return 0;
 }
