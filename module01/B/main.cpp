86428a1f75a28c7561c8c2dd03a54b3bfd70ccc1
Elisey Zubkov <zubkov.ei@phystech.edu>
2019-10-18 21:24:24 +0300

    module01:B: refactoring

--- module01/B/main.cpp
+++ module01/B/main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cassert>
 
-std::vector<int> StringToZ(const std::string& txt) {
-    int const txt_size = txt.size();
-    std::vector<int> z (txt_size, 0);
-
-    int left = 0, right = 0;
-    for (int i = 1; i < txt_size; ++i) {
-        int shift = i <= right ? std::min(z[i - left], right - i + 1) : 0;
-        while (shift + i < txt_size && txt[i + shift] == txt[shift]) {
-            ++shift;
+std::vector<int> StringToZ(const std::string &txt) {
+    std::vector<int> z(txt.length(), 0);
+    int const len_txt = txt.length();
+    int left = 0;
+    int right = 0;
+    for (int i = 1; i < len_txt; ++i) {
+        int curr = i <= right ? std::min(z[i - left], right - i + 1) : 0;
+        while (curr + i < len_txt && txt[i + curr] == txt[curr]) {
+            ++curr;
         }
-        z[i] = shift;
-        if (shift + i - 1 > right) {
+        z[i] = curr;
+        if (curr + i - 1 > right) {
             left = i;
-            right = shift + i - 1;
+            right = curr + i - 1;
         }
     }
     return z;
 }
 
-std::vector<int> StringToPrefix(const std::string& txt) {
-    int txt_size = txt.size();
-    std::vector<int> prefix (txt_size, 0);
-    for (int i = 1; i < txt_size; ++i) {
+std::vector<int> StringToPrefix(const std::string &txt) {
+    int len = txt.length();
+    std::vector<int> prefix(len, 0);
+    for (int i = 1; i < len; ++i) {
         int k = prefix[i - 1];
         while (k > 0 && txt[i] != txt[k]) {
             k = prefix[k - 1];
@@ -33,17 +35,16 @@ std::vector<int> StringToPrefix(const std::string& txt) {
     return prefix;
 }
 
-std::vector<int> PrefixToZ (const std::vector<int>& prefix) {
-    size_t prefix_size = prefix.size();
-    std::vector<int> z(prefix_size, 0);
-
-    for (int i = 1; i < prefix_size; i++) {
+std::vector<int> PrefixToZ(const std::vector<int> &prefix) {
+    std::vector<int> z(prefix.size());
+    for (int i = 1; i < prefix.size(); i++) {
         if (prefix[i] > 0) {
             z[i - prefix[i] + 1] = prefix[i];
         }
     }
-
-    for (int i = 1; i < prefix_size;) {
+    z[0] = prefix.size();
+    int i = 1;
+    while (i < prefix.size()) {
         int t = i;
         if (z[i] > 0) {
             for (int j = 1; j < z[i]; j++) {
@@ -56,34 +57,38 @@ std::vector<int> PrefixToZ (const std::vector<int>& prefix) {
         }
         i = t + 1;
     }
-
+    z[0] = 0;
     return z;
 }
 
-std::string ZToString(const std::vector<int>& z) {
+std::string ZToString(const std::vector<int> &z) {
+    if (z.size() == 0) {
+        return "";
+    }
     std::string txt = "a";
+    std::vector<int> used_indices;
     bool append_after_block = true;
-
-    for (int txt_index = 1; txt_index < z.size(); ++txt_index) {
-        std::vector<int> used_indices;
+    int txt_index = 1;
+    while (txt_index < z.size()) {
         if (z[txt_index] != 0) {
             used_indices.clear();
-
-            for (int prefix_index = 0, remaining_block_size = z[txt_index];
-                remaining_block_size > 0;
-                --remaining_block_size, ++prefix_index) {
-
-                if (z[txt_index] > remaining_block_size) {
-                    remaining_block_size = z[txt_index];
+            int prefix_index = 0;
+            int remaining_block_len = z[txt_index];
+            while (remaining_block_len > 0) {
+                if (z[txt_index] > remaining_block_len) {
+                    remaining_block_len = z[txt_index];
                     used_indices.push_back(z[txt_index]);
                     prefix_index = 0;
-                } else if (z[txt_index] == remaining_block_size) {
+                }
+                if (z[txt_index] == remaining_block_len) {
                     used_indices.push_back(z[txt_index]);
                 }
                 txt += txt[prefix_index];
+                ++prefix_index;
+                ++txt_index;
+                --remaining_block_len;
             }
             append_after_block = true;
-
         } else {
             if (append_after_block) {
                 std::vector<bool> used_chars(26, false);
@@ -105,10 +110,9 @@ std::string ZToString(const std::vector<int>& z) {
     return txt;
 }
 
-std::vector<int> ZToPrefix(std::vector<int>& z) {
-    const size_t z_size = z.size();
-    std::vector<int> prefix (z_size, 0);
-    for (int i = 1; i < z_size; ++i) {
+std::vector<int> ZToPrefix(const std::vector<int>& z) {
+    std::vector<int> prefix(z.size(), 0);
+    for (int i = 1; i < z.size(); ++i) {
         for (int j = z[i] - 1; j >= 0 && prefix[i + j] <= 0; --j) {
             prefix[i + j] = j + 1;
         }
@@ -116,32 +120,47 @@ std::vector<int> ZToPrefix(std::vector<int>& z) {
     return prefix;
 }
 
-char GetNewChar(const std::vector<int>& prefix, const std::string& txt, int pos) {
+char new_character(const std::vector<int> &prefix, const std::string &txt, int pos) {
     std::vector<bool> used(26, false);
-    for (int symbol = prefix[pos - 1]; symbol > 0;) {
+    int symbol = prefix[pos - 1];
+    while (symbol > 0) {
         used[txt[symbol] - 'a'] = true;
         symbol = prefix[symbol - 1];
     }
     char new_character = 'b';
-    for (;used[new_character - 'a']; ++new_character);
+    while (used[new_character - 'a']) {
+        ++new_character;
+    }
     return new_character;
-
 }
 
-std::string PrefixToString(std::vector<int> prefix) {
+std::string PrefixToString(const std::vector<int> &prefix) {
+    if (prefix.size() == 0) {
+        return "";
+    }
     std::string txt = "a";
     for (int i = 1; i < prefix.size(); ++i) {
         if (prefix[i] == 0) {
-            txt += GetNewChar(prefix, txt, i);
-        }
-        else {
+            txt += new_character(prefix, txt, i);
+        } else {
             txt += txt[prefix[i] - 1];
         }
     }
     return txt;
 }
 
-int main () {
+void CheckResult() {
+    std::string txt = "aabbabbaba";
+
+    assert((ZToString(PrefixToZ(StringToPrefix(txt)))) == txt);
+    assert(PrefixToString(ZToPrefix(StringToZ(txt))) == txt);
+
+    std::cout << "OK" << std::endl;
+}
+
+int main() {
+
+    CheckResult();
 
     return 0;
 }
